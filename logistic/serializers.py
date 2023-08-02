fa27c1f14668de32014fdd99919b04c769c7aebf
Kazakovtsev Nikita <nikita_kazakovtsev@bk.ru>
2023-08-02 13:09:22 +0300

    first commit

---
+++ logistic/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+
+from logistic.models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price', ]
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions', ]
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        for field_position in positions:
+            StockProduct.objects.create(
+                stock = stock, 
+                ** field_position,
+                )
+        
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for field_position in positions:
+            StockProduct.objects.update_or_create(
+                stock = stock,  
+                product = field_position['product'], 
+                defaults=dict(quantity=field_position['quantity'], price=field_position['price'])
+                )
+            
+        return stock
